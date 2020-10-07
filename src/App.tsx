168a0ab04a84f5e054103257cb14f2453a3a0169
ireshka <ireshka@users.noreply.github.com>
2020-10-07 05:54:31 +0200

    Change icon set to iconify
    - clean old fontawesome icons
    - add iconify & used icons dependencies
    - add all used icons to project
    - create object with used icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 // eslint-disable-next-line
 import React, { FC } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
 import Home from './pages/Home';
 import ErrorPage from './pages/404';
-
-library.add(fab);
 
 const App: FC = () => {
   return (
---
+++ src/theme/icons.tsx
@@ -0,0 +1,26 @@
+import { IconifyIcon } from '@iconify/react';
+import folder from '@iconify/icons-mdi/folder';
+import settings from '@iconify/icons-mdi/cog-outline';
+import trash from '@iconify/icons-mdi/delete';
+import edit from '@iconify/icons-clarity/note-edit-line';
+import arrowRight from '@iconify/icons-ic/outline-keyboard-arrow-right';
+import sync from '@iconify/icons-ic/baseline-sync';
+import arrowBack from '@iconify/icons-ic/round-arrow-back';
+import plus from '@iconify/icons-ant-design/plus-outlined';
+import github from '@iconify/icons-ant-design/github-filled';
+
+type TIcons = Record<string, IconifyIcon>;
+
+const icons: TIcons = {
+  folder,
+  settings,
+  trash,
+  edit,
+  arrowRight,
+  sync,
+  arrowBack,
+  plus,
+  github,
+};
+
+export default icons;
